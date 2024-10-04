14b4b45ca2c9286b7d82b03ba8d2620335add0e9
Khaled Sharaf Eldin <khaled.sharaf152@gmail.com>
2024-10-04 03:37:35 +0300

    fix commands

--- src/Commands/GenerateCrud.php
+++ src/Commands/GenerateCrud.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 
 class GenerateCrud extends Command
 {
-    protected $signature = 'fr:crud-generate {name?} {module?} {--force}';
+    protected $signature = 'crud:generate {name?} {--module=} {--force}';
     protected $description = 'Generate CRUD operations for a given model within a specified module based on config file';
 
     
@@ -19,23 +19,31 @@ class GenerateCrud extends Command
         if ($force) {
             $sureForce = $this->confirm('Are you sure you want to delete the existing files?');
             if (!$sureForce) {
-                $this->info("No CRUD generated.");
+                $this->alert('No CRUD generated.');
                 return;
             }
         }
-        $moduleName = $this->argument('module') ?? null;
+        $moduleName = $this->option('module') ?? null;
         $crudName = $this->argument('name') ? strtolower(Str::snake($this->argument('name'))) : null;
+        if ($crudName && !$moduleName) {
+            $this->print('error', 'Module name is required when generating a single CRUD.');
+            return;
+        }
         $crudGeneratorService = new CrudGeneratorService();
         $generated = $crudGeneratorService->generate($moduleName, $crudName, $force);
         if ($generated == 'single') {
-            $this->info("CRUD for {$crudName} in module {$moduleName} generated successfully.");
+            $this->print('info', "CRUD for {$crudName} in module {$moduleName} generated successfully.");
         } else if ($generated == 'all') {
-            $this->info("All CRUD generated successfully.");
+            $this->print('info', "All CRUD generated successfully.");
         } else {
-            $this->info("No CRUD generated.");
+            $this->alert('Not Found CRUD to generate.');
         }
     }
 
+    private function print(string $type, string $message): void
+    {
+        $this->$type("\n\n   {$message}\n");
+    }
     
 }
 
--- src/Commands/MakeCrud.php
+++ src/Commands/MakeCrud.php
@@ -11,35 +11,39 @@ use W88\CrudSystem\Models\Crud;
 
 class MakeCrud extends Command
 {
-    protected $signature = 'fr:crud-make {name} {module}';
+    protected $signature = 'crud:make {name} {--module=}';
     protected $description = 'Generate a CRUD configuration file for a specified module and model';
     protected $module;
     protected $modelName;
 
     public function handle()
     {
-        $this->module = $this->argument('module');
+        $this->module = $this->option('module');
         $crudName = strtolower(Str::snake($this->argument('name')));
         $this->modelName = Str::studly($crudName);
         $crudStubPath = __DIR__ . '/../stubs/crud.stub';
         $modulePath = base_path('Modules/' . $this->module . '/config/cruds');
+        if (!$this->module) {
+            $this->print('error', "Module name is required when generating a single CRUD.");
+            return;
+        }
 
         if (!File::exists($crudStubPath)) {
-            $this->error("Stub file not found at path: {$crudStubPath}");
+            $this->print('error', "Stub file not found at path: {$crudStubPath}.");
             return;
         }
 
         if (!Module::has($this->module)) {
-            $this->error("Module not found: {$this->module}");
+            $this->print('error', "Module not found: {$this->module}.");
             return;
         }
 
         $crud = Crud::whereName($crudName)->whereModule($this->module)->first();
         if ($crud) {
-            $this->error("{$crudName} CRUD already exists");
+            $this->print('error', "{$crudName} CRUD already exists.");
             return;
         }
-        $this->info("Creating config file for {$crudName} in module {$this->module}.");
+        $this->print('info', "Creating config file for {$crudName} in module {$this->module}.");
         $fileName = $this->getFileName($crudName);
         StubGenerator::from($crudStubPath, true)
                     ->withReplacers($this->getReplacers())
@@ -47,7 +51,7 @@ class MakeCrud extends Command
                     ->as($fileName)
                     ->save();
         Crud::newCrud($crudName, $fileName, $this->module);
-        $this->info("Config file for {$crudName} in module {$this->module} created successfully.");
+        $this->print('info', "Config file for {$crudName} in module {$this->module} created successfully.");
     }
 
     protected function getReplacers(): array
@@ -63,5 +67,10 @@ class MakeCrud extends Command
         $name = "create_{$name}_crud";
         $time = now()->format('Y_m_d_His');
         return "{$time}_{$name}";
+    }
+
+    private function print(string $type, string $message): void
+    {
+        $this->$type("\n\n   {$message}\n");
     }
 }
